b0b9ec6324d248ab73a4d3b81529b0de67ad870d
davidbabb <127700708+davidbabb@users.noreply.github.com>
2023-06-26 19:49:43 -0400

    created project and cleaned data

---
+++ baseball-analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A deep look into the statistics of baseball and how they can be used to best predict team success in a given season',
+    author='David Babb',
+    license='',
+)
